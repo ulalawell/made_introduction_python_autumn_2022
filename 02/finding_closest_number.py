cbd3a3ee94edaa59f69da35374fb7cc69386100d
ulalawell <roman2000.usik@gmail.com>
2022-09-25 17:34:47 +0300

    refactor hm2

---
+++ 02/finding_closest_number.py
@@ -0,0 +1,19 @@
+def find_closest_number_to_zero(numbers):
+    closest_numbers = []
+
+    if not numbers:
+        return closest_numbers
+    min_abs_value = abs(min(numbers, key=abs))
+
+    for element in numbers:
+        if abs(element) == min_abs_value:
+            closest_numbers.append(element)
+    return closest_numbers
+
+
+assert find_closest_number_to_zero([-5, 9, 6, -8]) == ([-5])
+assert find_closest_number_to_zero([-1, 2, -5, 1, -1]) == ([-1, 1, -1])
+assert find_closest_number_to_zero([5, 5, 5, -5, -5, -5]) == ([5, 5, 5, -5, -5, -5])
+assert find_closest_number_to_zero([]) == ([])
+
+print('ok')
--- 02/merging_subsequences.py
+++ 02/merging_subsequences.py
@@ -1,16 +1,3 @@
-def find_closest_number_to_zero(numbers):
-    closest_numbers = []
-
-    if not numbers:
-        return closest_numbers
-    min_abs_value = abs(min(numbers, key=abs))
-
-    for element in numbers:
-        if abs(element) == min_abs_value:
-            closest_numbers.append(element)
-    return closest_numbers
-
-
 def merge(subsequence1, subsequence2):
     ptr1 = 0
     ptr2 = 0
@@ -32,16 +19,10 @@ def merge(subsequence1, subsequence2):
     return merged_subsequence
 
 
-assert find_closest_number_to_zero([-5, 9, 6, -8]) == ([-5])
-assert find_closest_number_to_zero([-1, 2, -5, 1, -1]) == ([-1, 1, -1])
-assert find_closest_number_to_zero([5, 5, 5, -5, -5, -5]) == ([5, 5, 5, -5, -5, -5])
-assert find_closest_number_to_zero([]) == ([])
-
 assert merge([1, 1, 2, 5, 7], (1, 1, 2, 3, 4, 7)) == ([1, 2, 7])
 assert merge([1, 3, 5], ()) == ([])
 assert merge([], (1, 3, 5)) == ([])
 assert merge([], ()) == ([])
 assert merge([1, 1, 1, 1], (1, 1, 1)) == ([1])
-
 
 print('ok')
