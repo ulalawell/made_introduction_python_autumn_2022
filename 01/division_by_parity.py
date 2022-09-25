a2e28bfa08f517ba129f98793c3df583596d3355
ulalawell <roman2000.usik@gmail.com>
2022-09-25 17:31:32 +0300

    refactor hm1

--- 01/division_by_parity.py
+++ 01/division_by_parity.py
@@ -1,6 +1,3 @@
-import math
-
-
 def divide_by_parity(numbers):
     even_numbers = []
     odd_numbers = []
@@ -12,21 +9,8 @@ def divide_by_parity(numbers):
     return even_numbers, odd_numbers
 
 
-def find_roots(a, b, c):
-    determinant = b * b - 4 * a * c
-    if determinant < 0:
-        return None
-    first_root = (-b + math.sqrt(determinant)) / (2 * a)
-    second_root = (-b - math.sqrt(determinant)) / (2 * a)
-    return first_root, second_root
-
-
 assert divide_by_parity([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) == ([2, 4, 6, 8, 10], [1, 3, 5, 7, 9])
 assert divide_by_parity([1, 3, 5, 7, 9]) == ([], [1, 3, 5, 7, 9])
 assert divide_by_parity([2, 4, 6, 8, 10]) == ([2, 4, 6, 8, 10], [])
-
-assert find_roots(1, 2, -3) == (1, -3)
-assert find_roots(4, 4, 1) == (-0.5, -0.5)
-assert find_roots(1, 1, 10) is None
 
 print('ok')
---
+++ 01/equation_solution.py
@@ -0,0 +1,17 @@
+import math
+
+
+def find_roots(a, b, c):
+    determinant = b * b - 4 * a * c
+    if determinant < 0:
+        return None
+    first_root = (-b + math.sqrt(determinant)) / (2 * a)
+    second_root = (-b - math.sqrt(determinant)) / (2 * a)
+    return first_root, second_root
+
+
+assert find_roots(1, 2, -3) == (1, -3)
+assert find_roots(4, 4, 1) == (-0.5, -0.5)
+assert find_roots(1, 1, 10) is None
+
+print('ok')
