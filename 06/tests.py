18a409ec4411c7f48319dbce8c59e8e99d4f7e03
ulalawell <roman2000.usik@gmail.com>
2022-11-06 14:48:49 +0300

    Fix tests

--- 06/tests.py
+++ 06/tests.py
@@ -1,12 +1,7 @@
 import unittest
-from io import StringIO
-from unittest import mock
-import sys
-
 
 from LRUCache import LRUCache
 
-@mock.patch("sys.stdout", new=StringIO())
 class TestString(unittest.TestCase):
     def test_01(self):
         cache = LRUCache(2)
@@ -14,24 +9,17 @@ class TestString(unittest.TestCase):
         cache.set("k1", "val1")
         cache.set("k2", "val2")
 
-        print(cache.get("k3"))
-        print(cache.get("k2"))
-        print(cache.get("k1"))
+        self.assertEqual(cache.get("k3"), None)
+        self.assertEqual(cache.get("k2"), "val2")
+        self.assertEqual(cache.get("k1"), "val1")
 
         cache.set("k3", "val3")
 
-        print(cache.get("k3"))
-        print(cache.get("k2"))
-        print(cache.get("k1"))
+    
+        self.assertEqual(cache.get("k3"), "val3")
+        self.assertEqual(cache.get("k2"), None)
+        self.assertEqual(cache.get("k1"), "val1")
 
-        output = sys.stdout.getvalue().strip().splitlines()
-
-        self.assertEqual(output[0], "None")
-        self.assertEqual(output[1], "val2")
-        self.assertEqual(output[2], "val1")
-        self.assertEqual(output[3], "val3")
-        self.assertEqual(output[4], "None")
-        self.assertEqual(output[5], "val1")
 
     def test_02(self):
         cache = LRUCache(5)
@@ -42,36 +30,21 @@ class TestString(unittest.TestCase):
         cache.set("k4", "val4")
         cache.set("k5", "val5")
 
-        print(cache.get("k5"))
-        print(cache.get("k4"))
-        print(cache.get("k3"))
-        print(cache.get("k2"))
-        print(cache.get("k1"))
-
-
-        output = sys.stdout.getvalue().strip().splitlines()
-
-        self.assertEqual(output[6], "val5")
-        self.assertEqual(output[7], "val4")
-        self.assertEqual(output[8], "val3")
-        self.assertEqual(output[9], "val2")
-        self.assertEqual(output[10], "val1")
+        self.assertEqual(cache.get("k5"), "val5")
+        self.assertEqual(cache.get("k4"), "val4")
+        self.assertEqual(cache.get("k3"), "val3")
+        self.assertEqual(cache.get("k2"), "val2")
+        self.assertEqual(cache.get("k1"), "val1")
 
     def test_03(self):
         cache = LRUCache(1)
 
         cache.set("k1", "val1")
-        print(cache.get("k1"))
+        self.assertEqual(cache.get("k1"), "val1")
 
         cache.set("k2", "val2")
-        print(cache.get("k1"))
-        print(cache.get("k2"))
-
-        output = sys.stdout.getvalue().strip().splitlines()
-
-        self.assertEqual(output[11], "val1")
-        self.assertEqual(output[12], "None")
-        self.assertEqual(output[13], "val2")
+        self.assertEqual(cache.get("k1"), None)
+        self.assertEqual(cache.get("k2"), "val2")
 
 
     def test_04(self):
@@ -80,23 +53,19 @@ class TestString(unittest.TestCase):
         cache.set("k1", "val1")
         cache.set("k2", "val2")
 
-        print(cache.get("k1"))
-        print(cache.get("k2"))
+        self.assertEqual(cache.get("k1"), "val1")
+        self.assertEqual(cache.get("k2"), "val2")
 
         cache.set("k3", "val3")
         cache.set("k1", "val1")
+        self.assertEqual(cache.get("k1"), "val1")
+        self.assertEqual(cache.get("k2"), None)
+        self.assertEqual(cache.get("k3"), "val3")
+
         print(cache.get("k1"))
         print(cache.get("k2"))
         print(cache.get("k3"))
 
-
-        output = sys.stdout.getvalue().strip().splitlines()
-
-        self.assertEqual(output[14], "val1")
-        self.assertEqual(output[15], "val2")
-        self.assertEqual(output[16], "val1")
-        self.assertEqual(output[17], "None")
-        self.assertEqual(output[18], "val3")
 
 
 
